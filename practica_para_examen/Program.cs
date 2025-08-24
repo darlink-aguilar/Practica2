e603306c0f2d8a2a8e0bd3b24aa4ffb2c534bd51
Darlink <darlinkaguilargr@gmail.com>
2025-08-23 22:11:54 -0500

    Creacion de clases y objetos, utilizando herencia

--- practica_para_examen/Program.cs
+++ practica_para_examen/Program.cs
@@ -1,2 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Creamos el objeto de cliente y su instancia
+Clientes cliente1 = new Clientes();
+cliente1.Id = 1;
+cliente1.Nombre = "Darlibk";
+cliente1.Edad = 19;
+cliente1.Estatura = 1.52m;
+cliente1.Actividad = true;
+
+Console.WriteLine(cliente1.Id);
+Console.WriteLine(cliente1.Nombre);
+
+// Creamos el objeto de empleado y su instancia
+Empleados empelado1 = new Empleados();
+empelado1.Id = 2;
+empelado1.Nombre = "Juan Esteban";
+empelado1.Edad = 20;
+empelado1.Estatura = 1.73m;
+empelado1.Cargo = "Secretario";
+
+Console.WriteLine(empelado1.Id);
+Console.WriteLine(empelado1.Nombre);
+Console.WriteLine(empelado1.Cargo);
+
+// Creamos el objeto de autor y su instancia
+Autores autor1 = new Autores();
+autor1.Id = 3;
+autor1.Nombre = "Brayan";
+autor1.Nacimiento = DateTime.Now;
+
+Console.WriteLine(autor1.Id);
+Console.WriteLine(autor1.Nombre);
+Console.WriteLine(autor1.Nacimiento);
+
+// Creamos el objeto de libro y su instancia
+Libros libro1 = new Libros();
+libro1.Id = 102;
+libro1.Titulo = "Cien años de soledad";
+libro1.Autor = new Autores { Id = 3, Nombre = "Brayan" };
+
+Console.WriteLine(libro1.Id);
+Console.WriteLine(libro1.Autor); // Arreglar
+
+public class Libros
+{
+    public int Id;
+    public string? Titulo;
+    public Autores? Autor;
+}
+
+public class Usuarios
+{
+    public int Id;
+    public string? Nombre;
+    public int Edad;
+    public decimal Estatura;
+
+}
+
+public class Clientes : Usuarios
+{
+    public bool Actividad;
+}
+
+public class Empleados : Usuarios
+{
+    public string? Cargo;
+}
+
+public class Autores : Usuarios
+{
+    public DateTime Nacimiento;
+
+}
+
--- practica_para_examen/obj/Debug/net9.0/practica_para_examen.AssemblyInfo.cs
+++ practica_para_examen/obj/Debug/net9.0/practica_para_examen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practica_para_examen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e15005edbf19b7de0e4e1543951da187dd1599f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e30660b5b483290eaa52f46a7211f7caba9fed")]
 [assembly: System.Reflection.AssemblyProductAttribute("practica_para_examen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practica_para_examen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
